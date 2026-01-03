4322aa92876bcaf0a180cda242399779fe5b523c
Takuma Tsutsumi <kumata0615@icloud.com>
2026-01-03 13:06:40 +0900

    Finish user signup(7章終わり)

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,4 +13,33 @@ RSpec.describe "Users", type: :request do
     end 
   end
 
+  describe "POST /users #create" do
+    it "無効な値だと登録されないこと" do
+      expect {
+        post users_path, params { users: { name: "",
+                                           email: "user@invalid",
+                                           password: "foo",
+                                           password_confirmation: "bar" } }
+      }.to_not change(User, :count)
+    end
+  end
+
+  context "有効な値の場合" do
+    let(:user_params) { { user: { name: "Example User",
+                                  email: "user@example.com",
+                                  password: "password",
+                                  password_confirmation: "password" } } }
+
+    it "登録されること" do
+      expect {
+        post user_path, params: user_params
+      }.to change(User, :count).by 1
+    end
+    
+    it "users/showにリダイレクトされること" do
+      post users_path, params: user_params
+      user = User.last
+      expect(response).to redirect_to user
+    end
+  end
 end
